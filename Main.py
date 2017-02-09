64e44a26376ba35ebfaad8c88a8c9968b752cb87
Akatzuka <Svucinich@csumb.edu>
2017-02-09 07:24:54 +0000

    Finished

--- Main.py
+++ Main.py
@@ -1,19 +1,39 @@
-from __future__ import print_function
+#from __future__ import print_function
 import sys
+#import time
 from PIL import Image
-from PIL import PixelAccess
 
-im_1 = Image.open("./Project1Images/1.png")
-im_2 = Image.open("./Project1Images/2.png")
-im_3 = Image.open("./Project1Images/3.png")
-im_4 = Image.open("./Project1Images/4.png")
-im_5 = Image.open("./Project1Images/5.png")
-im_6 = Image.open("./Project1Images/6.png")
-im_7 = Image.open("./Project1Images/7.png")
-im_8 = Image.open("./Project1Images/8.png")
-im_9 = Image.open("./Project1Images/9.png")
+im_list = []
+for i in range(9):
+    im_list.append(Image.open("./Project1Images/" + str(i+1) + ".png"))
 
-for x in range (0, 494):
-    for y in range (0, 556):
+height, width = im_list[0].size
+
+im_final = Image.new("RGB", (height, width), color=0)
+
+list_of_pixels = []
+i = 0
+
+for x in range (0, (height - 1)):
+    for y in range (0, (width - 1)):
         for once in range (0,1):
-            pixel_list = {im_1(x,y), im_2(x,y),im_3(x,y), im_4(x,y),im_5(x,y), im_6(x,y),im_7(x,y), im_8(x,y), im_9(x,y)}
+            p_l_r = [im_list[0].getpixel((x,y))[0], im_list[1].getpixel((x,y))[0], im_list[2].getpixel((x,y))[0], im_list[3].getpixel((x,y))[0], im_list[4].getpixel((x,y))[0], im_list[5].getpixel((x,y))[0], im_list[6].getpixel((x,y))[0], im_list[7].getpixel((x,y))[0], im_list[8].getpixel((x,y))[0]] 
+            p_l_g = [im_list[0].getpixel((x,y))[1], im_list[1].getpixel((x,y))[1], im_list[2].getpixel((x,y))[1], im_list[3].getpixel((x,y))[1], im_list[4].getpixel((x,y))[1], im_list[5].getpixel((x,y))[1], im_list[6].getpixel((x,y))[1], im_list[7].getpixel((x,y))[1], im_list[8].getpixel((x,y))[1]] 
+            p_l_b = [im_list[0].getpixel((x,y))[2], im_list[1].getpixel((x,y))[2], im_list[2].getpixel((x,y))[2], im_list[3].getpixel((x,y))[2], im_list[4].getpixel((x,y))[2], im_list[5].getpixel((x,y))[2], im_list[6].getpixel((x,y))[2], im_list[7].getpixel((x,y))[2], im_list[8].getpixel((x,y))[2]] 
+
+            p_l_r.sort()
+            p_l_g.sort()
+            p_l_b.sort()
+            
+        list_of_pixels.append((p_l_r[5], p_l_g[5], p_l_b[5]))
+        p_l_r = []
+        p_l_g = []
+        p_l_b = []
+        
+for x in range (0, (height - 1)):
+    for y in range (0, (width - 1)):
+        for once in range (0,1):
+            im_final.putpixel((x, y), (list_of_pixels[i]))
+            i = i + 1
+            
+im_final.save("Final.png")
